38107cfb7d44b3ddb5a75dea74db9b18d57f03dd
dianasandrade2024 <dianasandrade@hotmail.com>
2025-03-20 00:54:50 -0300

    update commit gitpage

--- app/Http/Controllers/TestContact.php
+++ app/Http/Controllers/TestContact.php
@@ -11,7 +11,7 @@ class TestContact extends Controller
         $fakeData = [
             (['id'=> 1,'name' => "teste1",'email' => 'teste1@hotmail.com', 'telephone' => '5521998788218', 'subject' => 'Teste', 'mensage' => 'Marcar teste verifique e-mail']),
             (['id' => 2, 'name' => "teste2", 'email' => 'teste2@hotmail.com']),
-            (['id' => 3, 'name' => "teste3", 'email' => 'teste2@hotmail.com']),
+            (['id' => 3, 'name' => "teste3", 'email' => 'teste3@hotmail.com']),
         ];
         
         return response()->json( $fakeData);
